1e54dc2cea9428ed8a63dae3c7636289ed1ed58b
Colin Binion <colin.binion@me.com>
2018-07-27 22:56:04 -0600

    sanity save

--- client/src/components/blogs/BlogShow.js
+++ client/src/components/blogs/BlogShow.js
@@ -1,10 +1,28 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { fetchBlog } from '../../actions';
+import React, {
+  Component
+} from 'react';
+import {
+  connect
+} from 'react-redux';
+import {
+  fetchBlog
+} from '../../actions';
 
 class BlogShow extends Component {
   componentDidMount() {
     this.props.fetchBlog(this.props.match.params._id);
+  }
+
+  renderImage() {
+    if (this.props.blog.imageUrl) {
+      return ( <
+        img src = {
+          'https://s3-us-west-2.amazonaws.com/my-blog-bucket-123/' +
+          this.props.blog.imageUrl
+        }
+        />
+      );
+    }
   }
 
   render() {
@@ -12,19 +30,35 @@ class BlogShow extends Component {
       return '';
     }
 
-    const { title, content } = this.props.blog;
+    const {
+      title,
+      content
+    } = this.props.blog;
 
-    return (
-      <div>
-        <h3>{title}</h3>
-        <p>{content}</p>
-      </div>
+    return ( <
+      div >
+      <
+      h3 > {
+        title
+      } < /h3> <
+      p > {
+        content
+      } < /p> {
+        this.renderImage()
+      } <
+      /div>
     );
   }
 }
 
-function mapStateToProps({ blogs }, ownProps) {
-  return { blog: blogs[ownProps.match.params._id] };
+function mapStateToProps({
+  blogs
+}, ownProps) {
+  return {
+    blog: blogs[ownProps.match.params._id]
+  };
 }
 
-export default connect(mapStateToProps, { fetchBlog })(BlogShow);
+export default connect(mapStateToProps, {
+  fetchBlog
+})(BlogShow);
